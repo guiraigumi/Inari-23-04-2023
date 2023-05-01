0cc91eb62e5f7739aa6e89a7ce3a54fe50dddd70
guiraigumi <63981480+guiraigumi@users.noreply.github.com>
2023-05-01 20:54:18 +0200

    v.10

--- Assets/Dialogue_salacristales.cs
+++ Assets/Dialogue_salacristales.cs
@@ -79,7 +79,7 @@ public class Dialogue_salacristales : MonoBehaviour
             conversationEnded = true;
             bgmManager.StopBGM();
             objectToActivate.SetActive(true);
-            Invoke("AfterCinematic", 20.0f);
+            Invoke("AfterCinematic", 21.0f);
         }
     }
 
@@ -88,7 +88,7 @@ public class Dialogue_salacristales : MonoBehaviour
         Debug.Log("AFTER WAITING");
         if (conversationEnded)
         {
-            SceneManager.LoadScene("Scene_20");
+            SceneManager.LoadScene("Battle_Glava");
         }
     }
 
